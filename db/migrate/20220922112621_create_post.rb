23f0d69abe8e069d267a21b146d1a902dea580e3
MoRaad97 <89518488+MoRaad97@users.noreply.github.com>
2022-09-22 15:56:52 +0300

    Add tables

---
+++ db/migrate/20220922112621_create_post.rb
@@ -0,0 +1,13 @@
+class CreatePost < ActiveRecord::Migration[7.0]
+  def change
+    create_table :posts do |t|
+      t.string :title
+      t.text :text
+      t.integer :comment_counter
+      t.integer :likes_counter
+
+      t.timestamps
+    end
+    add_reference :posts, :users, null: false, foreign_key: true
+  end
+end
